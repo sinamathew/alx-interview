f43df244ab9e1f0f4685d1b96dc3bf00522c94b7
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-08-21 05:33:19 -0700

    task 0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-'''2D matrix rotation module.
-'''
+"""
+Rotate 2D Matrix
+"""
 
 
 def rotate_2d_matrix(matrix):
-    '''Rotates an m by n 2D matrix in place.
-    '''
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    """
+    Rotate a 2D matrix 90 degrees clockwise.
+    """
+    for i in range(int(len(matrix) / 2)):
+        for j in range(i, (len(matrix) - i - 1)):
+            x = (len(matrix) - 1 - j)
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[x][i]
+            matrix[x][i] = matrix[(len(matrix) - i - 1)][x]
+            matrix[(len(matrix) - i - 1)][x] = matrix[j][(len(matrix) - i - 1)]
+            matrix[j][(len(matrix) - i - 1)] = tmp
